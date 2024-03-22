ee4e4983d780289d974b95cd73f07cea0d2e4573
drendev <91354016+drendev@users.noreply.github.com>
2024-03-23 00:35:02 +0800

    Maintenance Page Testing

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,12 @@ export default function Home() {
       <Image src="/maintenance.svg" width={200} height={200} alt="EnginetifAI" />
       <header className="flex flex-col items-center">
         <h1 className="text-4xl font-bold mt-8 text-red-primary">EnginetifAI</h1>
-        <p className="text-lg text-center mt-4 text-gray-500">
+        <p className="text-lg text-center mt-4 text-gray-500 px-3">
           Start of a journey to develop AI powered Inventory Management System.
         </p>
       </header>
       <footer className="flex flex-col content-end mt-8">
-        <Image src={"/wave.svg"} width={10000} height={1000} alt="Dev Abi" />
+        <Image src={"/wave.svg"} width={10000} height={1000} alt="Waves" />
       </footer>
     </main>  
   );
