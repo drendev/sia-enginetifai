25932639b812504e93b60c64b1d496fd04d5c65a
drendev <91354016+drendev@users.noreply.github.com>
2024-03-23 01:20:28 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between pt-24">
+    <main className="flex min-h-full flex-col items-center justify-between pt-24">
       <Image src="/maintenance.svg" width={200} height={200} alt="EnginetifAI" />
       <header className="flex flex-col items-center">
         <h1 className="text-4xl font-bold mt-8 text-red-primary">EnginetifAI</h1>
@@ -11,7 +11,7 @@ export default function Home() {
           Start of a journey to develop AI powered Inventory Management System.
         </p>
       </header>
-      <footer className="flex flex-col content-end mt-8">
+      <footer className="absolute bottom-0 mt-8">
         <Image src={"/wave.svg"} width={10000} height={1000} alt="Waves" />
       </footer>
     </main>  
