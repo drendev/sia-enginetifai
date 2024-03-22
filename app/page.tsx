91ef188840314a6a48b3c0526ba18c6f20a40bf9
drendev <91354016+drendev@users.noreply.github.com>
2024-03-22 22:23:11 +0800

    Testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
 import Image from "next/image";
 
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between pt-24">
+      <Image src="/maintenance.svg" width={200} height={200} alt="EnginetifAI" />
       <header className="flex flex-col items-center">
         <h1 className="text-4xl font-bold mt-8 text-red-primary">EnginetifAI</h1>
-        <p className="text-lg text-center mt-4">
-          Start of a journey to create a better world with AI.
+        <p className="text-lg text-center mt-4 text-gray-500">
+          Start of a journey to develop AI powered Inventory Management System.
         </p>
       </header>
-      <footer className="flex flex-col items-center mt-8">
-        <p className="text-sm">
-          Dev Abi 2024
-        </p>
+      <footer className="flex flex-col content-end mt-8">
+        <Image src={"/wave.svg"} width={10000} height={1000} alt="Dev Abi" />
       </footer>
-    </main>
+    </main>  
   );
 }
