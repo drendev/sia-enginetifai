05e5f7ae8f1da059ab917082ab91f4899d2db0d2
drendev <91354016+drendev@users.noreply.github.com>
2024-03-23 11:59:49 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <Image src="/maintenance.svg" width={200} height={200} alt="EnginetifAI" />
       <header className="flex flex-col items-center">
         <h1 className="text-4xl font-bold mt-8 text-red-primary">EnginetifAI</h1>
-        <p className="text-lg text-center mt-4 text-gray-500 px-3">
+        <p className="text-lg mt-4 text-gray-500 px-6 text-justify">
           Start of a journey to develop AI powered Inventory Management System.
         </p>
       </header>
