ba53e229b835ed2acf25e76750bd594770ab66cf
drendev <91354016+drendev@users.noreply.github.com>
2024-03-24 00:28:51 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
       <header className="flex flex-col items-center">
         <h1 className="text-4xl font-bold mt-8 text-red-primary">EnginetifAI</h1>
         <p className="text-lg mt-4 text-gray-500 px-6 text-justify">
-        AI-Powered solution for Inventory Management System
+        AI-Powered solution for Inventory Management System. Coming Soon.
         </p>
       </header>
       <footer className="absolute bottom-0 mt-8">
