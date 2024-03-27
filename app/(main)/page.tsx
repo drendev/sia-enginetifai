6bc2abc5c59cd5c84ca266d18b6d111c7419f8ec
drendev <91354016+drendev@users.noreply.github.com>
2024-03-27 17:38:42 +0800

    Update page.tsx

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,11 +1,12 @@
 import Maintenance from "./@index/maintenance";
+import Index from "./@index/page";
 
 export default function Home() {
     const isLoggedIn = false;
     
     if (!isLoggedIn) {
   return (
-    <Maintenance />
+    <Index />
   );
     }
 }
