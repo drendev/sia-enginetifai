db5650a3d67784aad0a1900b47a5e3aaf28b9fcc
drendev <91354016+drendev@users.noreply.github.com>
2024-04-01 14:51:19 +0800

    Update page.tsx

--- app/(main)/@index/page.tsx
+++ app/(main)/@index/page.tsx
@@ -11,7 +11,7 @@ export default function Index() {
         <main className="min-h-full w-full flex flex-col bg-waves bg-no-repeat bg-cover">
         <div className="flex-1 flex flex-col items-center justify-center">
         <Header />
-        <div className="flex max-md:flex-wrap-reverse max-md:mt-10 max-md:gap-40 items-center justify-center relative md:gap-10 lg:gap-10">
+        <div className="flex max-md:flex-wrap-reverse max-md:mt-10 max-md:gap-32  ga-items-center justify-center relative md:gap-10 lg:gap-10">
         <Services />
         <Signin />
         </div>
