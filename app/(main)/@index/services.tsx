2429f586a1d5884bb8de30761c8e4b47cf84d7d7
drendev <91354016+drendev@users.noreply.github.com>
2024-04-01 15:33:40 +0800

    Landing Page: Services Pictures Updated

--- app/(main)/@index/services.tsx
+++ app/(main)/@index/services.tsx
@@ -14,31 +14,31 @@ const caveat = Caveat({ subsets: ["latin"] });
         <div className="flex flex-col w-96 justify-center relative md:mb-0 md:mr-10 bg-gray-primary rounded-full h-full">
             <Carousel autoplay style={contentStyle}>
                 <div className="justify-center mt-6">
-                <Image className="rounded-lg shadow-xl" src={'/example.webp'} width={350} height={250} alt=''/>
+                <Image className="rounded-lg" src={'/visual.svg'} width={350} height={250} alt=''/>
                 <span className={caveat.className}>
                 <p className="text-center text-4xl font-bold text-red-primary">Data Visualization</p>
                 </span>
                 </div>
                 <div className="justify-center mt-6">
-                <Image className="rounded-lg shadow-xl" src={'/example.webp'} width={350} height={250} alt=''/>
+                <Image className="rounded-lg" src={'/image-recog.svg'} width={350} height={250} alt=''/>
                 <span className={caveat.className}>
                 <p className="text-center text-4xl font-bold text-red-primary">Image Recognition</p>
                 </span>
                 </div>
                 <div className="justify-center mt-6">
-                <Image className="rounded-lg shadow-xl" src={'/example.webp'} width={350} height={250} alt=''/>
+                <Image className="rounded-lg" src={'/tracking.svg'} width={350} height={250} alt=''/>
                 <span className={caveat.className}>
                 <p className="text-center text-4xl font-bold text-red-primary">Delivery Tracking</p>
                 </span>
                 </div>
                 <div className="justify-center mt-6">
-                <Image className="rounded-lg shadow-xl" src={'/example.webp'} width={350} height={250} alt=''/>
+                <Image className="rounded-lg" src={'/secured.svg'} width={350} height={250} alt=''/>
                 <span className={caveat.className}>
                 <h1 className="text-center text-4xl font-bold text-red-primary">Secured System</h1>
                 </span>
                 </div>
                 <div className="justify-center mt-6">
-                <Image className="rounded-lg shadow-xl" src={'/example.webp'} width={350} height={250} alt=''/>
+                <Image className="rounded-lg" src={'/user-friendly.svg'} width={350} height={250} alt=''/>
                 <span className={caveat.className}>
                 <h1 className="font-bold text-4xl text-red-primary">User-Friendly</h1>
                 </span>
