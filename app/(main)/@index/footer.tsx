b39f27f2cc74344d3e61079229ae6353df446036
drendev <91354016+drendev@users.noreply.github.com>
2024-04-01 13:50:19 +0800

    Landing Page Responsiveness

--- app/(main)/@index/footer.tsx
+++ app/(main)/@index/footer.tsx
@@ -5,7 +5,7 @@ const caveat = Caveat({ subsets: ["latin"] });
 export default function Footer() {
     return (
         <>
-        <div className="absolute bottom-0 md:mt-0 md:mb-20">
+        <div className="absolute bottom-0 md:mt-0 md:mb-20 max-sm:hidden">
         <span className={caveat.className}>
         <h2 className="text-5xl text-white"> Future of Inventory Management System </h2>
         </span>
