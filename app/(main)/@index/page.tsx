9093f82daf61621c2f6e303fc7b05e28c732d1d7
drendev <91354016+drendev@users.noreply.github.com>
2024-04-01 14:28:36 +0800

    Update page.tsx

--- app/(main)/@index/page.tsx
+++ app/(main)/@index/page.tsx
@@ -8,16 +8,16 @@ import Footer from "./footer";
 export default function Index() {
     return (
         <>
-        <div className="min-h-full w-full flex flex-col bg-waves bg-no-repeat bg-cover">
-        <main className="flex-1 flex flex-col items-center justify-center">
+        <main className="min-h-full w-full flex flex-col bg-waves bg-no-repeat bg-cover">
+        <div className="flex-1 flex flex-col items-center justify-center">
         <Header />
-        <div className="flex max-md:flex-wrap-reverse max-md:mt-10 max-md:gap-10 items-center justify-center relative md:gap-10 lg:gap-10">
+        <div className="flex max-md:flex-wrap-reverse max-md:mt-10 max-md:gap-40 items-center justify-center relative md:gap-10 lg:gap-10">
         <Services />
         <Signin />
         </div>
         <Footer />
-        </main>
         </div>
+        </main>
         </>
     )
 }
