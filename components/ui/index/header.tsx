3f3f5a26939f615b1d9654a83b2a4c9b6f60911c
drendev <91354016+drendev@users.noreply.github.com>
2024-05-01 13:45:14 +0800

    Update header.tsx

--- components/ui/index/header.tsx
+++ components/ui/index/header.tsx
@@ -6,7 +6,7 @@ const caveat = Caveat({ subsets: ["latin"] });
 export function Header() {
     return(
     <>
-    <div className="top-0 absolute w-full p-2 md:p-6 shadow-lg bg-gray-primary">
+    <div className="top-0 absolute w-full p-2 md:p-4 shadow-lg bg-gray-primary">
         <a>
         <div className="flex flex-row ml-4">
         <Image src="/logo.png" width={60} height={60} alt=""/>
