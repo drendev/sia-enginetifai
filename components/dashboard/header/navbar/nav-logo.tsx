035893a6198c7d49d2b765e7f2939525c2855559
drendev <91354016+drendev@users.noreply.github.com>
2024-05-22 10:45:06 +0800

    nav utils

--- components/dashboard/header/navbar/nav-logo.tsx
+++ components/dashboard/header/navbar/nav-logo.tsx
@@ -6,7 +6,7 @@ export const NavLogo = () => {
         <Link href="/">
         <div className="flex ml-2 md:ml-5 py-3 justify-center">
         <Image className="w-auto md:h-8 h-6 self-center" src="/logo.png" width={60} height={50} alt=""/>
-        <h1 className="ml-2 md:ml-4 self-center font-bold tracking-wider text-sm md:text-lg text-slate-700">ENGINETIF<span className="text-red-900 font-bold">AI</span></h1>
+        <h1 className="ml-2 md:ml-4 self-center font-bold tracking-wider text-sm md:text-base lg:text-lg text-slate-700">ENGINETIF<span className="text-red-900 font-bold">AI</span></h1>
         </div>
         </Link>
     )
