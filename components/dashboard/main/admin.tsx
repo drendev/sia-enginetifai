6db00ac3bb6661544e024bb8ca309ba76b98768c
drendev <91354016+drendev@users.noreply.github.com>
2024-05-26 23:10:04 +0800

    Update admin.tsx

--- components/dashboard/main/admin.tsx
+++ components/dashboard/main/admin.tsx
@@ -1,9 +1,14 @@
+import Image from "next/image";
 
 export default async function AdminDashboard () {
   return(
     <>
       <div>
-        <h1>Admin Dashboard</h1>
+        <h1>Testing Page Speed</h1>
+        <Image src="/maintenance.svg" alt="admin" width={500} height={500} />
+        <Image src="/secured.svg" alt="admin" width={500} height={500} />
+        <Image src="/tracking.svg" alt="admin" width={500} height={500} />
+        <Image src="/visual.svg" alt="admin" width={500} height={500} />
       </div>
     </>
   )
