b2d404d0b172d7ade86ed3fea7aed67e9061721c
drendev <91354016+drendev@users.noreply.github.com>
2024-05-30 15:46:47 +0800

    update

--- components/dashboard/main/admin/firstgrid.tsx
+++ components/dashboard/main/admin/firstgrid.tsx
@@ -4,7 +4,15 @@ export function FirstGrid(){
     return(
         <>
         <div className="md:grid md:grid-rows-3 md:p-6 grid-cols-2 p-3 max-w-screen-xl mx-auto md:grid-flow-col flex flex-col gap-4">
-        <div className="md:row-span-16 md:col-span-1 h-full bg-red-primary/5 rounded-lg md:w-full">Charts & Data 
+        <div className="md:row-span-16 md:col-span-1 h-full bg-red-primary/5 rounded-lg md:w-full">
+            <div className="col-span-3 h-full space-y-4">
+                <div className="bg-red-primary/20 h-1/2 rounded-lg m-3">
+                    Statistics
+                </div>
+                <div className="h-1/2 bg-red-primary/20 rounded-lg m-3">
+                    Charts
+                </div>
+            </div>
         
         </div>
         <div className="md:col-span-2 md:h-32 bg-red-primary/5 rounded-lg">Engines</div>
