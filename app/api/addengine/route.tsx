3252ecd89b17e7f7a44e6e6d717d427d3dd64700
drendev <91354016+drendev@users.noreply.github.com>
2024-06-06 17:52:58 +0800

    Update route.tsx

--- app/api/addengine/route.tsx
+++ app/api/addengine/route.tsx
@@ -4,51 +4,45 @@ import { hash } from 'bcrypt';
 import { z } from 'zod';
 
 // Define schema
-const userSchema = z
+const engineSchema = z
   .object({
-    username: z.string().min(3, 'Username is required').max(30),
-    email: z.string().min(8, 'Email is required').email('Invalid email'),
-    password: z
-      .string()
-      .min(1, 'Password is required')
-      .min(8, 'Password must have than 8 characters'),  
-    role: z.string().min(1, 'Username is required').max(100),
+    userName: z.string().min(5, 'Username Max Limit.').max(30),
+    engineName: z.string().min(5, 'Engine Max Limit.').max(30),
+    engineType: z.string().min(8, 'Engine Type is required').max(100),
+    price: z.number().min(1, 'Price is required').max(100000),
+    quantity: z.number().min(1, 'Quantity is required').max(100),
+    picture: z.string().min(5, 'Picture is required').max(100),
+    description: z.string().min(5, 'Description is required').max(250),
   })
 
 export async function POST(req: Request) {
     try {
         const body = await req.json();
-        const { email, username, password, role } = userSchema.parse(body);
+        const { userName, engineName, engineType, price, quantity, picture, description } = engineSchema.parse(body);
 
-        // check if email is already in use
-        const existingUserByEmail = await db.user.findUnique({
-            where: { email: email}
-        })
-        const existingUserByUsername = await db.user.findUnique({
-            where: { username: username}
+        // check if engine is in use
+        const existingEngine = await db.engine.findUnique({
+            where: { engineName: engineName}
         })
 
-        if(existingUserByEmail){
-            return NextResponse.json({user: null, message: 'Email already in use'}, { status: 409 })
+        if(existingEngine){
+            return NextResponse.json({user: null, message: 'Engine already existed.'}, { status: 409 })
         }
-        if(existingUserByUsername){
-            return NextResponse.json({user: null, message: 'User already exist'}, { status: 409 })
-        }
-        const hashedPassword = await hash(password, 10)
 
-        const newUser = await db.user.create({
+        const newEngine = await db.engine.create({
             data: {
-                username,
-                email,
-                password: hashedPassword,
-                role
+                userName,
+                engineName,
+                engineType,
+                price,
+                quantity,
+                picture,
+                description,
             }
         });
-        const {password: newUserPassword, ...rest} = newUser;
 
         return NextResponse.json({
-            user: rest, 
-            message: 'User created'},
+            message: 'Engine added successfully.'},
             {status: 201}
         )
     }
