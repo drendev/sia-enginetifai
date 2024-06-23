cf2d01943ec29496d09c5e6d7db80cf8c404716e
drendev <91354016+drendev@users.noreply.github.com>
2024-06-24 00:02:44 +0800

    Update route.tsx

--- app/api/testapi/route.tsx
+++ app/api/testapi/route.tsx
@@ -1,42 +1,18 @@
+
 import { db } from '@/lib/db';
 import { NextResponse } from 'next/server';
 
-export async function GET(request:Request) {
-    if (request.method === 'OPTIONS') {
-        return new NextResponse(null, {
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'GET,OPTIONS',
-                'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-            }
-        });
-    }
+export async function GET() {
+    const enginePrice = await db.user.findMany({
+        select: {
+            id: true,
+            username: true,
+            email: true,
+            password: true,
+            role: true,
+        }
+    });
 
-    try {
-        const users = await db.user.findMany({
-            select: {
-                id: true,
-                username: true,
-                email: true,
-                password: true,
-                role: true,
-            }
-        });
+    return NextResponse.json(enginePrice);
 
-        return new NextResponse(JSON.stringify(users), {
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*'
-            }
-        });
-    } catch (error) {
-        console.error("Error fetching users:", error);
-        return new NextResponse(JSON.stringify({ error: 'Internal Server Error' }), {
-            status: 500,
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*'
-            }
-        });
-    }
 }
