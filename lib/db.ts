a27747f0cf4857fa8c5fc6c7c0d7801baa674533
drendev <91354016+drendev@users.noreply.github.com>
2024-06-23 20:39:11 +0800

    query database bug

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,13 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = global as unknown as {
-    prisma: PrismaClient | undefined;
+declare global {
+  // Allow the use of `global` as the namespace for Prisma
+  // This is important for avoiding issues with Next.js hot reloading
+  var prisma: PrismaClient | undefined;
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+const prisma = global.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
 
 export const db = prisma;
