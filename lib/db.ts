2aa173c85e898880acbe71dd29b6236ec229ad15
drendev <91354016+drendev@users.noreply.github.com>
2024-06-23 17:40:14 +0800

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,15 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-// Extending the global object to include a PrismaClient instance
-const globalForPrisma = global as typeof global & {
-    prisma?: PrismaClient;
+const globalForPrisma = global as unknown as {
+    prisma: PrismaClient | undefined;
 }
 
-// Create a new PrismaClient instance if it doesn't exist
 export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
-// In development, store the PrismaClient instance on the global object to avoid creating multiple instances during hot-reloading
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
-// Export the PrismaClient instance for use in your application
 export const db = prisma;
