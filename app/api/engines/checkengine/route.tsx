976e26f9aace25b0b59efcfa029287cb2fa3b25b
drendev <91354016+drendev@users.noreply.github.com>
2024-07-03 16:34:38 +0800

    db disconnect

--- app/api/engines/checkengine/route.tsx
+++ app/api/engines/checkengine/route.tsx
@@ -21,5 +21,7 @@ export async function POST(req: Request) {
     } catch (error) {
         console.error("Error fetching engine price:", error);
         return NextResponse.json({ error: 'Internal Server Error' });
+    } finally {
+        await db.$disconnect();
     }
 }
