142dadd85de368eaf07e4c8251b3a9b97815f1ba
drendev <91354016+drendev@users.noreply.github.com>
2024-07-13 07:37:31 +0800

    Update form.tsx

--- components/imagerecognition/form.tsx
+++ components/imagerecognition/form.tsx
@@ -51,7 +51,7 @@ const Upload: React.FC<UploadProps> = ({ onFileSelect }) => {
             isDragActive ? 'border-red-primary/60' : 'border-red-primary'
           }`}
         >
-          <input {...getInputProps()} />
+          <input {...getInputProps()} capture="environment" />
           {selectedImage ? (
             <>
               <img src={selectedImage} alt="Selected" className="max-w-full h-36 mx-auto" />
