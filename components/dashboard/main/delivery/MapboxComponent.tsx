faa43a47d69654a5a1a3cf8dc02d8df58e2ed548
drendev <91354016+drendev@users.noreply.github.com>
2024-07-22 06:34:15 +0800

    Map Tracking

--- components/dashboard/main/delivery/MapboxComponent.tsx
+++ components/dashboard/main/delivery/MapboxComponent.tsx
@@ -139,7 +139,7 @@ const MapboxExample: React.FC = () => {
       if (coordinates) {
         submitCoordinates(coordinates.latitude, coordinates.longitude);
       }
-    }, 1500);
+    }, 1000);
 
     return () => clearInterval(interval);
   }, [coordinates, user]);
