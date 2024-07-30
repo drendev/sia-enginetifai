82b779f92e4a5931876fedc13d16e5d13dbca505
drenswe <91354016+drendev@users.noreply.github.com>
2024-07-30 16:02:22 +0800

    Update MapboxComponent.tsx

--- components/dashboard/main/delivery/MapboxComponent.tsx
+++ components/dashboard/main/delivery/MapboxComponent.tsx
@@ -139,7 +139,7 @@ const MapboxExample: React.FC = () => {
       if (coordinates) {
         submitCoordinates(coordinates.latitude, coordinates.longitude);
       }
-    }, 700);
+    }, 50);
 
     return () => clearInterval(interval);
   }, [coordinates, user]);
