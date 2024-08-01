65a349bb136c5de0622152dd04949f804162d688
drendev <91354016+drendev@users.noreply.github.com>
2024-08-01 14:02:46 +0800

    Update page.tsx

--- app/(dashboard)/(admin)/deliverytracking/page.tsx
+++ app/(dashboard)/(admin)/deliverytracking/page.tsx
@@ -1,7 +1,9 @@
+import Link from 'next/link'
 
 export default function DeliveryTracking() {
     return (
         <div className="h-full py-16">
+            <Link href="/deliverytracking/delivery/70"> Map Beta Transaction ID: 70 </Link>
         </div>
     )
 }
