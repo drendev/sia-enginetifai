ddf04ccecb5aadf6ff78dda51c1f74024cb27cea
hiromutech <148634375+hiromutech@users.noreply.github.com>
2024-08-02 22:36:40 +0800

    Update page.tsx

--- app/(dashboard)/(admin)/staff/page.tsx
+++ app/(dashboard)/(admin)/staff/page.tsx
@@ -395,8 +395,8 @@ export default function Employees() {
         />
         <div className="flex">
           <div className="mt-3 flex-grow">
-            {filteredUsers.length - 1 < 0 ? filteredUsers.length : filteredUsers.length}{" "}
-            {filteredUsers.length === 2 || filteredUsers.length === 1
+            {filteredUsers.length - 1 === -1 ? "0" : filteredUsers.length - 1}{" "}
+            {filteredUsers.length - 1 === 1
               ? "Staff"
               : "Staffs"}
           </div>
@@ -566,7 +566,7 @@ export default function Employees() {
             </Form>
           </Modal>
         </div>
-        {/* Kupal */}
+
         {showEditStaffForm && (
           <div className="fixed inset-0 flex items-center justify-center z-50 bg-gray-900 bg-opacity-50 overflow-scroll md:overflow-auto">
             <div className="bg-white dark:bg-gray-800 p-6 rounded-lg mt-16 shadow-lg w-full max-w-4xl">
@@ -656,15 +656,15 @@ export default function Employees() {
                         }
                         options={[
                           {
-                            value: "Admin",
+                            value: "admin",
                             label: "Admin",
                           },
                           {
-                            value: "Employee",
+                            value: "employee",
                             label: "Employee",
                           },
                           {
-                            value: "Courier",
+                            value: "courier",
                             label: "Courier",
                           },
                         ]}
@@ -791,18 +791,6 @@ export default function Employees() {
                       <span className="ml-1 text-sm text-gray-500 dark:text-gray-400">
                         {user.role}
                       </span>
-                      <div className="flex mt-3 ml-1">
-                        <p className="text-gray-400 text-xs flex-grow">
-                          Department
-                        </p>
-                        <p className="text-gray-400 text-xs mr-2">Date Hired</p>
-                      </div>
-                      <div className="flex mt-3 ml-1">
-                        <p className="text-white text-xs flex-grow">
-                          (Department)
-                        </p>
-                        <p className="text-white text-xs mr-2">(Date)</p>
-                      </div>
                       <div className="flex mt-5 ml-1">
                         <svg
                           className="w-3.5 h-3.5 me-2"
@@ -814,31 +802,14 @@ export default function Employees() {
                         >
                           <path
                             d="M72 0H8C3.6 0 0.04 3.6 0.04 8L0 56C0 60.4 3.6 64 8 64H72C76.4 64 80 60.4 80 56V8C80 3.6 76.4 0 72 0ZM72 16L40 36L8 16V8L40 28L72 8V16Z"
-                            fill="white"
+                            fill="black"
                           />
                         </svg>
-                        <p className="text-white text-xs mr-2 text-right flex-grow">
+                        <p className="text-black text-xs mr-2 text-right flex-grow">
                           {user.email}
                         </p>
                       </div>
-                      <div className="flex mt-3 ml-1">
-                        <svg
-                          className="w-3.5 h-3.5 me-2"
-                          width="72"
-                          height="72"
-                          viewBox="0 0 72 72"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            d="M14.48 31.16C20.24 42.48 29.52 51.72 40.84 57.52L49.64 48.72C50.72 47.64 52.32 47.28 53.72 47.76C58.2 49.24 63.04 50.04 68 50.04C70.2 50.04 72 51.84 72 54.04V68C72 70.2 70.2 72 68 72C30.44 72 0 41.56 0 4C0 1.8 1.8 0 4 0H18C20.2 0 22 1.8 22 4C22 9 22.8 13.8 24.28 18.28C24.72 19.68 24.4 21.24 23.28 22.36L14.48 31.16Z"
-                            fill="white"
-                          />
-                        </svg>
-                        <p className="text-white text-xs mr-2 text-right flex-grow">
-                          9953715230
-                        </p>
-                      </div>
+            
                     </div>
                   </div>
                 ) : null
